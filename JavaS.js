4403622fdc6fd93ffbd03481ffce0c501c986fbd
Jbsteward1 <88251372+Jbsteward1@users.noreply.github.com>
2021-11-21 22:19:49 -0500

    website idea walkthrough

---
+++ JavaS.js
@@ -0,0 +1,33 @@
+/**CALLING A FUNCTION
+ * A function allows you to write a block of,
+ * code that performs specific functionality. 
+ * It is written like like:
+ */
+//  function sum(x, y) {
+//     return x + y;
+//     // code after the return will not run
+//   }
+//   var sumOfNumbers = sum(5, 7);
+//   console.log(sumOfNumbers); // 12
+
+
+  /** If conditionals
+   * For writing conditional statements if, else if, else
+   * keywords are used and the comparison logic is wrapped in parenthesis
+   * */
+
+ let name = "Jeremy"
+  if ( name == "Jeremy") {
+      console.log("You have won the prize")
+  } else if ( name == "Ron") {
+      console.log("Thats also correct")
+          else{
+              console.log("Sorry I Dont Know You")
+          
+      }
+  }
+
+
+
+
+
