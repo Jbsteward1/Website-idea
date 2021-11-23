a33fcff9d185ea0ef6055e6fd1f8fd22c3ffbec7
Jbsteward1 <88251372+Jbsteward1@users.noreply.github.com>
2021-11-23 13:54:57 -0500

    finished styling now adding the javascript needed to make it functional

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Targetting items
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar_menu')
-
+const navlogo = document.querySelector('#navbar__logo')
 // Display mobile menu
 //Creat a function that adds a class that is either active or is active
 // arrow function
@@ -12,3 +12,18 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu)
+
+// Show active enu when scrolling
+
+const highlightMenu = () => {
+    const elem = document.querySelector('highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const servicesMenu= document.querySelector('#services-page')
+    let scrollPos = window.scrollY
+
+
+    // adds highlight class to my menu items
+
+    if(window.innerWidth > 960 && scrollPos < 600)
+}
